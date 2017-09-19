d1335ff93fd8b80e7b0c846da0dde3eb891f53b2
asra <asra@netcompany.com>
2017-09-19 19:15:35 +0200

    Initial commit - OpenAI Gym Tutorial

---
+++ Gym/Tutorial.py
@@ -0,0 +1,84 @@
+import gym
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+env = gym.make('CartPole-v0')
+
+## Save/Remember: current action, previous action, current observation, previous observation
+rewardsforplotting = []
+
+policy = [np.random.uniform(-1,1),np.random.uniform(-1,1),np.random.uniform(-1,1), np.random.uniform(-1,1)]
+def Learn(currentPolicy, previousPolicy, currentReward, previousBestReward):
+	if (currentReward > previousBestReward):
+		return list(currentPolicy), currentReward, True
+	return list(previousPolicy), previousBestReward, False
+
+
+def Act(state, policy):
+	preaction = 0
+	for j in range(4):
+		preaction += policy[j]*state[j]
+
+	action = int(round(1/(1+np.exp(-preaction))))
+	return action
+
+bestRewardSoFar = 0
+currentBestPolicy = policy
+for i_episode in range(1000):
+	observation = env.reset()
+
+
+	
+	accumulatedReward = 0
+	currentTrialPolicy = list(currentBestPolicy) #We have to make a copy here. list() does this.
+	#Vary policy somehow
+	scalefactor = np.random.uniform(0.8, 1.2)
+	randindex = np.random.randint(4)
+	signchange = int((round(np.random.random())-0.5)*2)
+	currentTrialPolicy[randindex] *= scalefactor*signchange
+	for ack in range(10):
+		observation = env.reset()
+		for t in range(200):
+			##env.render()
+			action = Act(observation, currentTrialPolicy)
+			observation, reward, done, info = env.step(action)
+			accumulatedReward += reward
+			
+			#time.sleep(0.5)
+			if done:
+				#print("Episode finished after {} timesteps".format(t+1))
+				break
+			
+	currentBestPolicy, bestRewardSoFar, updated = Learn(currentTrialPolicy, currentBestPolicy, accumulatedReward, bestRewardSoFar)
+	
+	
+	#print(policy)
+	# updated = False
+	# for j in range(len(policy)):
+	# 	if (abs(currentBestPolicy[j]-currentTrialPolicy[j])<10**-5):
+	# 		updated = True
+	if (updated):
+		rewardsforplotting.append(accumulatedReward)
+	#print("Updated?", updated)
+
+
+
+
+plt.plot(rewardsforplotting)
+plt.show()
+
+##Render the best policy
+observation = env.reset()
+for t in range(200):
+	env.render()
+	action = Act(observation, currentBestPolicy)
+	observation, reward, done, info = env.step(action)
+	if done:
+		break;
+print("Finished after " + str(t+1) + " timesteps.")
+
+#Data: currentBestPolicy, currentTrialPolicy, currentReward, currentBestReward
+# currentTrialPolicy <- Vary(currentBestPolicy)
+# Run environment: currentReward += observed reward at each timestep
+# currentBestPolicy <- currentReward > currentBestReward ? currentTrialPolicy : currentBestPolicy
+# currentBestReward = max(currentReward, currentBestReward)
